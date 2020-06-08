be32abd47cca54f04769f73543923aae11b21efe
blvrn <65164774+blvrn@users.noreply.github.com>
2020-06-08 17:05:12 +0300

    Update 001.py

--- 001.py
+++ 001.py
@@ -1,5 +1,9 @@
 #in the name of the Allah
 
+""" Problems
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
 total = 0                # initial total value
  
 for number in range(1,1000):  # For all positive numbers to 1000 

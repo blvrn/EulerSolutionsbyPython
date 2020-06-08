d22f3a6bbfdeb9327393ef578a8af6bb90db330a
blvrn <65164774+blvrn@users.noreply.github.com>
2020-06-08 17:07:56 +0300

    Update 004.py

--- 004.py
+++ 004.py
@@ -1,5 +1,12 @@
 # In the name of Allah
 
+"""Problems
+
+A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+Find the largest palindrome made from the product of two 3-digit numbers.
+
+"""
+
 
 n = 0
 for a in range(999, 100, -1):

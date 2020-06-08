c41f3b50abb114d2eb57b1c3e515aad2a7384c77
blvrn <65164774+blvrn@users.noreply.github.com>
2020-06-08 17:07:21 +0300

    Update 003.py

--- 003.py
+++ 003.py
@@ -1,5 +1,12 @@
 # In the name of Allah
 
+"""Problems
+
+The prime factors of 13195 are 5, 7, 13 and 29.
+What is the largest prime factor of the number 600851475143 ?
+
+"""
+
 
 n = 600851475143
 
